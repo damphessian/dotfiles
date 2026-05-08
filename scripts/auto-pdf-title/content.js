41a70048b051d6113acebb6dbce83492c71f6200
Jacob Romer <h35514n@icloud.com>
2026-05-08 06:48:18 -0400

    refactor: Relocate unrelated utilities

--- scripts/auto-pdf-title/content.js
+++
@@ -1,9 +0,0 @@
-console.log('[auto-pdf-title] Loading...');
-
-(function() {
-    console.log(`[auto-pdf-title] Title originally: ${document.title}`);
-
-    const topic = document.querySelector('#topicName').textContent;
-    document.title = topic;
-    console.log(`[auto-pdf-title] Title updated to: ${document.title}`);
-})();
--- scripts/crampuswire/background.js
+++
@@ -1,3 +0,0 @@
-chrome.runtime.onMessage.addListener(({ bookmark }, sender, sendResponse) => {
-  chrome.bookmarks.create(bookmark, (bookmark) => {});
-});
