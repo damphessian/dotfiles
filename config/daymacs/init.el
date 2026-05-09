cc7309b6737512fdc441cf431f0350a283e5986b
Jacob Romer <h35514n@icloud.com>
2026-05-09 09:29:42 -0400

    fix[emacs]: Fix flymake for config dir
    - Trust the config directory so Flymake will work
    - Compile key paths eagerly to make them available to the compiler
    - Use `dm-config-root' as the canonical path

--- config/daymacs/init.el
+++ config/daymacs/init.el
@@ -7,8 +7,18 @@
 
 ;;; Code:
 
-;; Load config directory
-(add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
+(eval-and-compile
+  (defconst dm-config-root (file-name-as-directory (file-truename "~/.config/daymacs")))
+  ;; Trust config directory
+  (add-to-list 'trusted-content (abbreviate-file-name dm-config-root))
+  ;; Add submodule directory to load path
+  (add-to-list 'load-path (expand-file-name "config/" dm-config-root))
+  ;; For Flymake: Add straight.el build directories to load path at compile time
+  (let ((build-dir (expand-file-name "straight/build/" dm-config-root)))
+    (when (file-directory-p build-dir)
+      (dolist (dir (directory-files build-dir t "\\`[^.]"))
+        (when (file-directory-p dir)
+          (add-to-list 'load-path dir))))))
 
 ;; Enable logging
 (require 'dm-log)
